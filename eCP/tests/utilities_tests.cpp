533d5d93f1491ebf606694d6c5915768ef79e589
Morten Skoett <mortenskoett@gmail.com>
2021-03-17 20:48:39 +0100

    Modify get_random_unique_indexes function
    Now collects points in a vector a returns that. Simplifies how to work
    with the function at the cost of double memory usage.

--- eCP/tests/utilities_tests.cpp
+++ eCP/tests/utilities_tests.cpp
@@ -1,5 +1,6 @@
 ﻿#include <gtest/gtest.h>
 #include <eCP/utilities/utilities.hpp>
+#include <algorithm>
 
 /* Tests */
 
@@ -9,14 +10,15 @@ TEST(utilities_tests, get_random_unique_indexes_given_9_10_returns_set_with_size
 }
 
 TEST(utilities_tests, get_random_unique_indexes_given_10_10_returns_complete_set) {
-  std::vector<int> expected_numbers {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<unsigned> expected_numbers {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
   auto result_set = utilities::get_random_unique_indexes(10, 10);
 
   EXPECT_EQ(result_set.size(), 10);
 
   for (auto& num : expected_numbers) {
-    EXPECT_TRUE(result_set.find(num) != result_set.end());
+    auto it = std::find(result_set.begin(), result_set.end(), num);
+    EXPECT_TRUE(it != result_set.end());
   }
 }
 
@@ -25,8 +27,6 @@ TEST(utilities_tests, get_random_unique_indexes_given_0_10_returns_0_elements_in
   EXPECT_EQ(result_set.size(), 0);
 }
 
-// FIXME: Consider not asserting in function an be able to use this test.
-//TEST(utilities_tests, get_random_unique_indexes_given_10_5_returns_0_elements_in_set) {
-//  auto result_set = utilities::get_random_unique_indexes(10, 5);
-//  EXPECT_EQ(result_set.size(), 0);
-//}
+TEST(utilities_tests, get_random_unique_indexes_given_6_5_throws_invalid_argument_exception) {
+  EXPECT_THROW(utilities::get_random_unique_indexes(6, 5), std::invalid_argument);
+}
