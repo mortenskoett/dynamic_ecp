519abb5cdc80e5b25d6922630378cf762d2a2315
Morten Skoett <mortenskoett@gmail.com>
2021-04-19 18:20:33 +0200

    Add initial implementation of maintenance module
    - This implementation is non-recursive and contains a bit of redundancy
    of code because cluster and nodes must be handled similarly but not
    identically. Internal index Nodes work only with children of type Node
    and Nodes that are by definition clusters (bottom level / level L) work
    with children of type Point.
    
    - The insertion works by first collecting a set of pointers to the
    closest parents through the index. Then the given descriptor is inserted
    into the found cluster. Next it is checked whether a reclustering of the
    subtree in which the found cluster resides is necessary. If yes then the
    subtree will be reclustered using its parent. After this it will be
    checked whether the parent of the reclustered subtree also needs a
    reclustering because it might have grown, and the process is repeated
    all the way to the root node. For the root node, case a reclustering is
    needed and there is no root node then the index will be grown and it is
    checked whther a recluster is still required in which case on final
    reclustering of the children of the root will happen. Otherwise the
    process ends.
    
    - ReclusteringPolicies are ways to determine the threshold to split on:
    - AVERAGE: For a node n all n.children.children.size() is collected and
    compared to the expected average size for n.children().size().
    - ABSOLUTE: The size of the children list for any one Node is used as
    splitting threshold.

---
+++ eCP/src/eCP/index/maintenance.cpp
@@ -0,0 +1,275 @@
+#include <cassert>
+#include <eCP/index/maintenance.hpp>
+#include <eCP/index/shared/traversal.hpp>
+#include <eCP/utilities/utilities.hpp>
+#include <stack>
+#include <stdexcept>
+
+namespace maintenance_helpers {
+
+/**
+ * @brief grow_index
+ * @brief grow_index creates a new root node and replaces the current root in the Index type.
+ * The old root is added as child of the new.
+ * @param current_root is the old root that will be substituted.
+ * @param index is the index worked on.
+ * @return a pointer to the new root Node owned and stored by the Index.
+ */
+Node* grow_index(Node* current_root, Index* const index)
+{
+  // Pick single random leader from children of current root to be the new root.
+  const auto random_index = utilities::get_random_unique_indexes(1, current_root->children.size()).front();
+  auto new_root = current_root->children.at(random_index);
+
+  // Insert new root into index.
+  new_root.children.emplace_back(*current_root);  // Add old root to the children list.
+  index->root = new_root;
+  index->L++;
+
+  return &index->root;
+}
+
+void recluster_internal_node(Node* const parent, unsigned internal_node_threshold)
+{
+  std::vector<Node*> children;  // Total number of children of all children under parent.
+  children.reserve(internal_node_threshold * parent->children.size());
+
+  for (auto& parent_child : parent->children) {  // Collect all parent->children->children
+    for (auto& child : parent_child.children) {
+      children.emplace_back(&child);
+    }
+  }
+
+  // New level size is l = n / sn.
+  const unsigned n = children.size();
+  const unsigned l = std::ceil(n / static_cast<float>(internal_node_threshold));  // Floating point arith.
+  const auto indexes = utilities::get_random_unique_indexes(l, n);
+
+  std::vector<Node> leaders;
+  leaders.reserve(l);
+
+  for (unsigned index : indexes) {  // Pick l random new Nodes as leaders.
+    leaders.emplace_back(*children[index]);
+  }
+
+  // Add each node to its nearest parent
+  for (Node* node : children) {
+    traversal::find_nearest_leaf(node->get_leader()->descriptor, leaders)->children.emplace_back(*node);
+  }
+
+  parent->children.swap(leaders);
+}
+
+/**
+ * @brief count_children counts the children of all nodes on the given level.
+ * @param parent of the nodes containing the points that will be counted.
+ * @return a tuple containing the total number of children and the size of the largest children list
+ * encountered.
+ */
+unsigned count_children_for_children_of(const Node* parent)
+{
+  unsigned children_amount{0};
+
+  for (auto& child : parent->children) {
+    children_amount += child.children.size();
+  }
+  return children_amount;
+}
+
+bool is_internal_node_reclustering_required(Node* const leader, Node* const parent, ReclusteringPolicy policy,
+                                            unsigned hi_mark)
+{
+  switch (policy) {
+    case ReclusteringPolicy::ABSOLUTE: {
+      if (leader->children.size() > hi_mark) {
+        return true;
+      }
+      break;
+    }
+
+    case ReclusteringPolicy::AVERAGE: {
+      auto subtree_max{parent->children.size() * hi_mark};  // Theoretical max size based on hi watermark.
+      auto subtree_actual{count_children_for_children_of(parent)};
+
+      if (subtree_actual > subtree_max) {
+        return true;
+      }
+
+      break;
+    }
+
+    default:
+      throw std::invalid_argument("maintenance: The given ReclusteringPolicy is invalid.");
+  }
+  return false;
+}
+
+void recluster_cluster(Node* const cluster_parent, unsigned desired_cluster_size)
+{
+  std::vector<Point*> descriptors;  // Total number of descriptors of all children under parent.
+  descriptors.reserve(desired_cluster_size * cluster_parent->children.size());
+
+  for (auto& cluster : cluster_parent->children) {  // Collect all descriptors as pointers.
+    for (auto& point : cluster.points) {
+      descriptors.emplace_back(&point);
+    }
+  }
+
+  const unsigned n = descriptors.size();
+  const unsigned l = std::ceil(n / static_cast<float>(desired_cluster_size));  // Floating point arith.
+  const auto indexes = utilities::get_random_unique_indexes(l, n);
+
+  std::vector<Node> leaders;  // Revised set of leaders to substitute the children of parent.
+  leaders.reserve(l);
+
+  for (unsigned index : indexes) {  // Pick l random leaders from set of descriptors.
+    auto node = Node{*descriptors[index]};
+    node.points.reserve(desired_cluster_size);
+    leaders.emplace_back(node);
+    descriptors[index] = nullptr;  // Because it will make it easier to circumvent duplicates below.
+  }
+
+  for (Point* point : descriptors) {  // Redistribute all points to the new closest clusters.
+    if (point) {                      // Using nullptr to not re-add points used in Nodes above.
+      traversal::find_nearest_leaf(point->descriptor, leaders)->points.emplace_back(*point);
+    }
+  }
+
+  cluster_parent->children.swap(leaders);
+}
+
+/**
+ * @brief count_descriptors counts the descriptors of all nodes on a given level.
+ * @param parent of the clusters containing the points that will be counted.
+ */
+unsigned count_descriptors_for_children_of(const Node* parent)
+{
+  unsigned descriptors_amount{0};
+
+  for (auto& cluster : parent->children) {
+    descriptors_amount += cluster.points.size();
+  }
+  return descriptors_amount;
+}
+
+/**
+ * @brief is_cluster_reclustering_required computes whether a recluster is necessary based on the given
+ * ReclusteringPolicy. Will pop top element from the stack.
+ * @param path is the stack to the cluster into which a new descriptor has been inserted.
+ * @param policy is the current cluster ReclusteringPolicy.
+ * @param sc_threshold is the given desired cluster size Sc of the index.
+ * @returns true if a recluster is required and false otherwise.
+ */
+bool is_cluster_reclustering_required(Node* const cluster, Node* const parent, ReclusteringPolicy policy,
+                                      unsigned hi_mark)
+{
+  switch (policy) {
+    case ReclusteringPolicy::ABSOLUTE: {
+      if (cluster->points.size() > hi_mark) {
+        return true;
+      }
+      break;
+    }
+
+    case ReclusteringPolicy::AVERAGE: {
+      auto subtree_max{parent->children.size() * hi_mark};  // Theoretical max based on hi watermark.
+      auto subtree_actual{count_descriptors_for_children_of(parent)};
+
+      if (subtree_actual > subtree_max) {
+        return true;
+      }
+
+      break;
+    }
+
+    default:
+      throw std::invalid_argument("maintenance: The given ReclusteringPolicy is invalid.");
+  }
+
+  return false;
+}
+
+std::stack<Node*> collect_path_to_nearest_cluster(const float* query, Node* const root,
+                                                  std::stack<Node*> parents = std::stack<Node*>{})
+{
+  parents.emplace(root);
+
+  if (!root->children.empty()) {
+    Node* closest_child = traversal::get_closest_node(root->children, query);
+    return collect_path_to_nearest_cluster(query, closest_child, parents);
+  }
+
+  return parents;
+}
+
+/**
+ * @brief must_index_grow determines whether the index must grow at this point.
+ * @param path is the stack of pointers to each level in the index.
+ * @param threshold_sn is a threshold determining a maximum internal node size.
+ * @return true if the index must grow and false otherwise.
+ */
+bool must_index_grow(Node* root, unsigned sn_threshold)
+{
+  // It only makes sense to check children of root using Absolute strategy.
+  return (root->children.size() > sn_threshold);
+}
+
+void orchestrate_index_reclustering(std::stack<Node*>& path, Index* index)
+{
+  const unsigned max_size = index->scheme.hi_mark;
+  const unsigned desired_size = index->scheme.lo_mark;
+
+  // 1 -- Handle cluster
+  auto cluster = path.top();  // The cluster that just been added to.
+  path.pop();                 // Pop cluster to access leader.
+  auto leader = path.top();   // Valid b/c there will always be a root node initially.
+
+  if (!is_cluster_reclustering_required(cluster, leader, index->scheme.cluster_policy, max_size)) {
+    return;
+  }
+
+  recluster_cluster(leader, desired_size);
+
+  // 2 -- Handle nodes in index
+  // Recluster internal nodes above the cluster and below the root node.
+  while (path.size() > 1) {
+    path.pop();  // Pop the previous parent to get next.
+    auto leader_parent = path.top();
+
+    if (!is_internal_node_reclustering_required(leader, leader_parent, index->scheme.node_policy, max_size)) {
+      return;
+    }
+
+    recluster_internal_node(leader_parent, desired_size);
+    leader = leader_parent;  // Leader will point to the leader below on next iteration.
+  }
+
+  // 3 -- Handle root node
+  // This code will only be called if the root is reached i.e. when path.size() == 1.
+  assert(path.size() == 1 && "maintenance: Path stack should only contain a single root Node* here.");
+
+  auto current_root = path.top();
+
+  if (must_index_grow(current_root, desired_size)) {
+    auto new_root = grow_index(current_root, index);
+    recluster_internal_node(new_root, desired_size);
+  }
+}
+
+}  // namespace maintenance_helpers
+
+namespace maintenance {
+
+void insert(const float* descriptor, Index* index)
+{
+  if (index->size < 1)
+    throw std::invalid_argument(
+        "maintenance: It is required that the index contains at least a root node in order to insert.");
+
+  auto path = maintenance_helpers::collect_path_to_nearest_cluster(descriptor, &index->root);
+  path.top()->points.emplace_back(Point{descriptor, index->size++});  // Insert descriptor and incr. size.
+
+  maintenance_helpers::orchestrate_index_reclustering(path, index);
+}
+
+}  // namespace maintenance
---
+++ eCP/tests/maintenance_tests.cpp
@@ -0,0 +1,347 @@
+#include <gtest/gtest.h>
+
+#include <eCP/index/pre-processing.hpp>
+#include <eCP/index/shared/distance.hpp>
+#include <eCP/index/shared/globals.hpp>
+#include <helpers/testhelpers.hpp>
+
+// Testing compilation unit
+#include <eCP/index/maintenance.cpp>
+
+/*
+ * Helpers -------------------------------------
+ */
+
+// WIP
+
+// Index get_empty_default_index() {}
+
+// Not used
+//  std::vector<std::vector<float>> dataset{
+//      {1, 1, 1}, {2, 2, 2}, {3, 3, 3}, {4, 4, 4},    {5, 5, 5},    {6, 6, 6},
+//      {7, 7, 7}, {8, 8, 8}, {9, 9, 9}, {10, 10, 10}, {11, 11, 11}, {12, 12, 12},
+//  };
+
+/*
+ * Creates a L1 index with 1 cluster with 3 points.
+ */
+Index get_test_index_A()
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  auto policy = ReclusteringPolicy::AVERAGE;
+  auto const sc = 2;
+
+  // L1 index w. 1 cluster w. 3 points
+  auto level_1 = Node{Point{{2, 2, 2}, 2}};
+  level_1.points.emplace_back(Point{{3, 3, 3}, 3});
+  level_1.points.emplace_back(Point{{0, 0, 0}, 0});
+
+  auto root = Node{Point{{0, 0, 0}, 0}};
+  root.children.emplace_back(level_1);
+
+  // Create scheme
+  auto scheme = ReclusteringScheme{sc, sc, policy, policy};
+
+  // Create index.
+  auto index = Index{};
+  index.L = 1;
+  index.size = 3;
+  index.root = root;
+  index.scheme = scheme;
+
+  return index;
+}
+
+/*
+ * maintenance_tests below --------------------------------------
+ */
+
+TEST(
+    maintenance_tests,
+    insert_given_descriptor_and_premade_12descriptor_L2_index_with_Sc2_AverageStrategy_inserts_descriptor_and_reclusters_no_index_growth)
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  auto descriptor = Node{Point{new float[3]{42, 42, 42}, 1}};
+
+  std::vector<std::vector<float>> dataset{
+      {1, 1, 1}, {2, 2, 2}, {3, 3, 3}, {4, 4, 4},    {5, 5, 5},    {6, 6, 6},
+      {7, 7, 7}, {8, 8, 8}, {9, 9, 9}, {10, 10, 10}, {11, 11, 11}, {12, 12, 12},
+  };
+
+  //  auto index = pre_processing::create_index(dataset, sc, policy, policy);
+  auto index = get_test_index_A();
+  auto result = testhelpers::measure_depth_from(index.root);
+  ASSERT_EQ(index.size, 3);
+  ASSERT_EQ(result, 1);
+
+  // Act
+  maintenance::insert(descriptor.get_leader()->descriptor, &index);
+
+  // Assert
+  EXPECT_EQ(index.size, 4);
+  EXPECT_EQ(index.L, 1);
+}
+
+TEST(
+    maintenance_tests,
+    insert_given_descriptor_and_valid_minimal_index_Sc1_AbsoluteStrategy_inserts_descriptor_reclusters_without_growing_index)
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  auto policy = ReclusteringPolicy::ABSOLUTE;
+  auto sc = 1;
+  auto descriptor = Node{Point(new float[3]{4, 4, 4}, 1)};
+  std::vector<std::vector<float>> dataset = {{5, 5, 5}};
+
+  Index* index = pre_processing::create_index(dataset, sc, policy, policy, 0.3, 0.3);
+  ASSERT_EQ(index->size, 1);
+
+  // Act
+  maintenance::insert(descriptor.get_leader()->descriptor, index);
+
+  // Assert
+  EXPECT_EQ(index->size, 2);
+  EXPECT_EQ(index->L, 1);
+}
+
+TEST(
+    maintenance_tests,
+    insert_given_descriptor_and_valid_minimal_index_Sc2_AbsoluteStrategy_inserts_descriptor_no_reclustering_no_index_growth)
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  auto policy = ReclusteringPolicy::ABSOLUTE;
+  auto sc = 2;
+  auto descriptor = Node{Point(new float[3]{4, 4, 4}, 1)};
+  std::vector<std::vector<float>> dataset = {{5, 5, 5}};
+
+  Index* index = pre_processing::create_index(dataset, sc, policy, policy);
+  ASSERT_EQ(index->size, 1);
+
+  // Act
+  maintenance::insert(descriptor.get_leader()->descriptor, index);
+
+  // Assert
+  EXPECT_EQ(index->size, 2);
+}
+
+/*
+ * maintenance_helpers_tests below -------------------------------------
+ */
+
+TEST(maintenance_helpers_tests,
+     collect_path_to_nearest_clusters_given_3L_index_root_returns_stack_with_4_node_ptrs)
+{
+  // arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  unsigned sc = 2;
+  auto query = Node{Point(new float[3]{4, 4, 4}, 1)};
+
+  std::vector<std::vector<float>> dataset{
+      {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}, {10, 11, 12}, {10, 11, 12},
+      {2, 2, 3}, {2, 5, 6}, {2, 8, 9}, {2, 11, 12},  {2, 11, 12},  {2, 11, 12},
+  };
+
+  // act
+
+  // First built and validate index to validate test
+  auto index = pre_processing::create_index(dataset, sc);
+  auto assert_result = testhelpers::measure_depth_from(index->root);
+  ASSERT_EQ(assert_result, 3);  // This must hold for this test to be valid.
+
+  // assert
+  auto root = &index->root;
+  auto result = maintenance_helpers::collect_path_to_nearest_cluster(query.get_leader()->descriptor, root);
+  EXPECT_EQ(result.size(), 4);
+}
+
+TEST(maintenance_helpers_tests,
+     collect_path_to_nearest_clusters_given_single_root_node_returns_1_node_ptr_in_stack)
+{
+  // arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  auto root = Node{Point(new float[3]{4, 4, 4}, 1)};
+
+  // act
+  auto result = maintenance_helpers::collect_path_to_nearest_cluster(root.get_leader()->descriptor, &root);
+
+  // assert
+  EXPECT_EQ(result.size(), 1);
+}
+
+TEST(maintenance_helpers_tests, count_descriptors_given_parent_with_2_cluster_with_2_points_each_returns_4)
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+
+  auto p1 = Point{new float[3]{1, 1, 1}, 1};
+  auto p2 = Point{new float[3]{2, 2, 2}, 2};
+
+  Node root{p1};
+  root.children.emplace_back(Node{p2});
+  root.children.emplace_back(Node{p2});
+
+  // Add no 2 descriptor to each cluster
+  root.children.front().points.emplace_back(p2);
+  root.children.back().points.emplace_back(p2);
+
+  // Act
+  auto result = maintenance_helpers::count_descriptors_for_children_of(&root);
+  EXPECT_EQ(result, 4);
+}
+
+TEST(maintenance_helpers_tests,
+     is_cluster_reclustering_necessary_given_L2_stack_and_ABS_policy_returns_true_correct_number_of_children)
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  unsigned sc = 2;
+  auto policy = ReclusteringPolicy::ABSOLUTE;
+  auto query = Node{Point(new float[3]{4, 4, 4}, 1)};
+
+  auto p1 = Point{new float[3]{1, 1, 1}, 1};
+  auto p2 = Point{new float[3]{2, 2, 2}, 2};
+
+  Node root{p1};
+  root.children.emplace_back(Node{p2});
+  root.children.front().points.emplace_back(p2);
+  root.children.front().points.emplace_back(p2);
+
+  // Act
+  auto stack = maintenance_helpers::collect_path_to_nearest_cluster(query.get_leader()->descriptor, &root);
+  ASSERT_EQ(stack.size(), 2);
+
+  auto cluster = stack.top();
+  stack.pop();
+  auto parent = stack.top();
+  stack.pop();
+
+  // Assert
+  auto result = maintenance_helpers::is_cluster_reclustering_required(cluster, parent, policy, sc);
+
+  // Total number of descriptors collected as descendants of parent.
+  EXPECT_EQ(result, true);
+}
+
+TEST(
+    maintenance_helpers_tests,
+    is_cluster_reclustering_necessary_given_L2_Sc3_AVG_policy_with_some_cluster_larger_than_Sc_but_average_less_than_Sc_returns_false_0)
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  unsigned sc = 2;
+  auto policy = ReclusteringPolicy::AVERAGE;
+  auto query = Node{Point(new float[3]{4, 4, 4}, 1)};
+
+  auto p1 = Point{new float[3]{1, 1, 1}, 1};
+  auto p2 = Point{new float[3]{2, 2, 2}, 2};
+
+  // Average will be 5/3 = average less than sc.
+  Node root{p1};
+  root.children.emplace_back(Node{p2});
+  root.children.emplace_back(Node{p2});
+  root.children.emplace_back(Node{p2});
+  root.children.front().points.emplace_back(p2);  // Has 3 points.
+  root.children.front().points.emplace_back(p2);
+  // Act
+
+  // Create stack
+  auto stack = maintenance_helpers::collect_path_to_nearest_cluster(query.get_leader()->descriptor, &root);
+  ASSERT_EQ(stack.size(), 2);
+
+  auto cluster = stack.top();
+  stack.pop();
+  auto parent = stack.top();
+  stack.pop();
+
+  // Assert
+  auto result = maintenance_helpers::is_cluster_reclustering_required(cluster, parent, policy, sc);
+
+  // Total number of descriptors collected as descendants of parent.
+  EXPECT_EQ(result, false);
+}
+
+TEST(
+    maintenance_helpers_tests,
+    is_cluster_reclustering_necessary_given_L2_Sc3_AVG_policy_with_some_cluster_larger_than_Sc_total_larger_than_Sc_returns_true_amounof_children)
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  unsigned sc = 2;
+  auto policy = ReclusteringPolicy::AVERAGE;
+  auto query = Node{Point(new float[3]{4, 4, 4}, 1)};
+
+  auto p1 = Point{new float[3]{1, 1, 1}, 1};
+  auto p2 = Point{new float[3]{2, 2, 2}, 2};
+
+  // Average will be 5/3 = average less than sc.
+  Node root{p1};
+  root.children.emplace_back(Node{p2});
+  root.children.emplace_back(Node{p2});
+  root.children.front().points.emplace_back(p2);
+  root.children.front().points.emplace_back(p2);
+  root.children.front().points.emplace_back(p2);
+
+  // Act
+
+  // Create stack
+  auto stack = maintenance_helpers::collect_path_to_nearest_cluster(query.get_leader()->descriptor, &root);
+  ASSERT_EQ(stack.size(), 2);
+
+  auto cluster = stack.top();
+  stack.pop();
+  auto parent = stack.top();
+  stack.pop();
+
+  // Assert
+  auto result = maintenance_helpers::is_cluster_reclustering_required(cluster, parent, policy, sc);
+
+  // Total number of descriptors collected as descendants of parent.
+  EXPECT_EQ(result, true);
+}
+
+TEST(
+    maintenance_helpers_tests,
+    is_cluster_reclustering_necessary_given_L2_stack_and_ABS_policy_with_threshold_smaller_than_size_returns_false_0)
+{
+  // Arrange
+  distance::set_distance_function(distance::Metric::EUCLIDEAN_OPT_UNROLL);
+  globals::g_vector_dimensions = 3;
+  unsigned sc = 2;
+  auto policy = ReclusteringPolicy::ABSOLUTE;
+  auto query = Node{Point(new float[3]{4, 4, 4}, 1)};
+
+  auto p1 = Point{new float[3]{1, 1, 1}, 1};
+  auto p2 = Point{new float[3]{2, 2, 2}, 2};
+
+  Node root{p1};
+  root.children.emplace_back(Node{p2});
+
+  // Act
+
+  // Create stack
+  auto stack = maintenance_helpers::collect_path_to_nearest_cluster(query.get_leader()->descriptor, &root);
+  ASSERT_EQ(stack.size(), 2);
+
+  auto cluster = stack.top();
+  stack.pop();
+  auto parent = stack.top();
+  stack.pop();
+
+  // Assert
+  auto result = maintenance_helpers::is_cluster_reclustering_required(cluster, parent, policy, sc);
+
+  EXPECT_EQ(result, false);
+}
