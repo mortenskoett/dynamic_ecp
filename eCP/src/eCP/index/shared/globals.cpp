0bbcc6fa24b3084c2c5368f572b0aa8683afb125
Morten Skoett <mortenskoett@gmail.com>
2021-04-19 14:44:48 +0200

    Change get_random_unique_indexes to take unsigned ints as argument

--- eCP/src/eCP/index/shared/globals.cpp
+++ eCP/src/eCP/index/shared/globals.cpp
@@ -5,6 +5,9 @@ namespace globals {
 const float FLOAT_MAX = std::numeric_limits<float>::max();
 const float FLOAT_MIN = std::numeric_limits<float>::min();
 
+/**
+ * @brief g_vector_dimensions is the dimensionality of the dataset.
+ */
 unsigned int g_vector_dimensions;
 
 }  // namespace globals
