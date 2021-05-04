94ab0968cdad7c62695b71ef968f4709c6e4e1e8
Morten Skoett <mortenskoett@gmail.com>
2021-05-04 22:02:02 +0200

    Add working ann bencmarks files

---
+++ scripts/ecp_install.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/env bash
+
+# Script to install and build eCP such that it can be run locally from ann-benchmarks dir. 
+# It is assumed that the 'ann-benchmarks' and the 'eCP' repositories are located in the same 
+# dir './' and that this script is called from './eCP/scripts.
+
+# Attention: It is necessary to set REPO_DIR_NAME variable to the name you have
+# given the eCP repository.
+
+# Example:
+# ./ 
+#   ann-benchmarks/
+#   eCP/
+#     scripts/
+
+REPO_DIR_NAME="eCP"
+NAME="ecp_install"
+
+echo "${NAME}: Will generate wrapper, copy relevant ann-benchmarks files + generated wrapper."
+read -p "${NAME}: Press enter to continue"
+
+echo "${NAME}: Generating wrapper"
+./gen_wrapper.sh
+
+cd ../..
+
+[ ! -d ann-benchmarks ] \
+    && echo "${NAME}: Cloning new ann-benchmarks repo" \
+    && git clone https://github.com/erikbern/ann-benchmarks
+
+echo "${NAME}: Copy eCP necessary files"
+yes | cp -rv ${REPO_DIR_NAME}/benchmarks-files/eCP.py					ann-benchmarks/ann_benchmarks/algorithms/eCP.py
+yes | cp -rv ${REPO_DIR_NAME}/benchmarks-files/Dockerfile.ecp ann-benchmarks/install/Dockerfile.ecp
+yes | cp -rv ${REPO_DIR_NAME}/benchmarks-files/algos.yaml			ann-benchmarks/algos.yaml
+
+
+echo "${NAME}: Copying generated files"
+yes | cp -rv ${REPO_DIR_NAME}/eCP/build/swig/_eCP_wrapper.so ann-benchmarks/
+yes | cp -rv ${REPO_DIR_NAME}/eCP/build/swig/eCP_wrapper.py ann-benchmarks/
+
+echo "${NAME}: Setup python 3.6 env inside ann-benchmarks"
+cd ann-benchmarks
+python3.6 -m venv env
+source env/bin/activate
+
+echo "${NAME}: Install dependencies"
+pip install -r requirements.txt
+
+#echo "Building ONLY eCP docker image"
+#python install.py --algorithm ecp
+
+echo "${NAME}: Done."
---
+++ scripts/ecp_run_local.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+
+# Script to be copied into ann-benchmarks dir to automatically run and move around benchmarks
+# for absolute convenience. Uncomment dataset to be run.
+
+NAME="ecp_run_local"
+ALGO="eCP123"
+
+DATASET="random-xs-20-euclidean"
+# DATASET="glove-25-angular"
+
+echo "${NAME}: Will run benchmarks on ${ALGO}. First run 'ecp_install.sh'. Dataset: $DATASET."
+read -p "${NAME}: Press enter to continue"
+
+# Copy saved dataset if it exists
+FILE="../datasets/${DATASET}.hdf5"
+if [[ -f ${FILE} ]]; then
+  echo "${NAME}: Copy datasets"
+  yes | cp -rv ${FILE} ../../ann-benchmarks/data/${DATASET}.hdf5
+fi
+
+# Go to ann-benchmarks dir
+cd ../../ann-benchmarks
+
+echo "${NAME}: Setup python 3.6 env inside ann-benchmarks"
+python3.6 -m venv env
+source env/bin/activate
+
+echo "${NAME}: Running tests"
+python run.py --local --algorithm ${ALGO} --dataset ${DATASET}
+
+echo "${NAME}: Creating plots and website"
+python plot.py --dataset ${DATASET}
+
+mkdir -p website
+python create_website.py --outputdir website --scatter
+
+# echo "Cleaning up tests"
+
+echo "${NAME}: Done."
